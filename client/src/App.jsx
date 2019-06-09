9040b074fe1018a6c8ff7e520db0be352200f238
Rodolfo Perez <rodolfobeste@gmail.com>
2019-06-09 19:12:16 +0300

    Added react-router and created category route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter, Route, Redirect } from 'react-router-dom';
 import Header from './components/header';
 import MainJumbotron from './components/jumbotron';
 import ResultsContainer from './containers/resultsContainer';
@@ -6,11 +7,27 @@ import './app.css';
 
 const App = () => {
   return (
-    <div>
-      <Header />
-      <MainJumbotron />
-      <ResultsContainer />
-    </div>
+    <BrowserRouter>
+      <div>
+        <Header />
+        <Route path="/" exact render={() => <Redirect to="/events" />} />
+        <Route
+          path="/:category"
+          render={({ match }) =>
+            match.params.category === 'events' ||
+            match.params.category === 'festivals' ? (
+              <React.Fragment>
+                <MainJumbotron match={match} />
+                <ResultsContainer />
+              </React.Fragment>
+            ) : (
+              // Should be redirected to not found route
+              <Redirect to="/events" />
+            )
+          }
+        />
+      </div>
+    </BrowserRouter>
   );
 };
 
