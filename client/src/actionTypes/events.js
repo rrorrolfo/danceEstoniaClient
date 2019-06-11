e4bccf64c62dc4d321c053e71e9c57aee97b76da
Rodolfo Perez <rodolfobeste@gmail.com>
2019-06-11 23:43:57 +0300

    Optimized action types naming

--- client/src/actionTypes/events.js
+++ client/src/actionTypes/events.js
@@ -1,4 +1,4 @@
-export const FETCH_EVENTS = 'FETCH_EVENTS';
-export const FETCH_STARTED = 'FETCH_STARTED';
-export const FETCH_SUCCESS = 'FETCH_SUCCESS';
-export const FETCH_FAILURE = 'FETCH_FAILURE';
+export const FETCH_EVENTS = 'events/FETCH_EVENTS';
+export const FETCH_STARTED = 'events/FETCH_STARTED';
+export const FETCH_SUCCESS = 'events/FETCH_SUCCESS';
+export const FETCH_FAILURE = 'events/FETCH_FAILURE';
