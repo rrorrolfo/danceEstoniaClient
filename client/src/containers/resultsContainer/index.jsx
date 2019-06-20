2a64e63b93df394c1ee8511848c818b53f6ea4ac
Rodolfo Perez <rodolfobeste@gmail.com>
2019-06-20 23:43:32 +0300

    Added display of festivals funcitonality

--- client/src/containers/resultsContainer/index.jsx
+++ client/src/containers/resultsContainer/index.jsx
@@ -3,34 +3,52 @@ import React, { useEffect } from 'react';
 import { Container } from 'react-bootstrap';
 import { connect } from 'react-redux';
 import { fetchEvents } from '../../actions/events';
+import { fetchFestivals } from '../../actions/festivals';
 import NavigationTabs from '../../components/tabs';
 import ResultCard from '../../components/resultCard';
 
-const ResultsContainer = ({ getEvents, events }) => {
+const ResultsContainer = ({
+  getEvents,
+  getFestivals,
+  events,
+  festivals,
+  match
+}) => {
   useEffect(() => {
-    getEvents();
+    if (match.params.category === 'events') {
+      getEvents();
+    } else {
+      getFestivals();
+    }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const displayResults = events.map(event => (
-    <ResultCard result={event} key={event._id} />
-  ));
+  const displayResults = results => {
+    return results.map(result => (
+      <ResultCard result={result} key={result._id} />
+    ));
+  };
 
   return (
     <Container>
       <NavigationTabs />
-      <Container className="results-container">{displayResults}</Container>
+      <Container className="results-container">
+        {match.params.category === 'events'
+          ? displayResults(events)
+          : displayResults(festivals)}
+      </Container>
     </Container>
   );
 };
 
 const mapStateToProps = state => {
-  return { events: state.events.events };
+  return { events: state.events.events, festivals: state.festivals.festivals };
 };
 
 const mapDispatchToProps = dispatch => {
   return {
-    getEvents: () => dispatch(fetchEvents())
+    getEvents: () => dispatch(fetchEvents()),
+    getFestivals: () => dispatch(fetchFestivals())
   };
 };
 
