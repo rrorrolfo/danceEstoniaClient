d11110ba8cb0f327dbde385bcd21e6856a94b860
Rodolfo Perez <rodolfobeste@gmail.com>
2019-06-21 23:05:44 +0300

    Connected component to Redux

--- client/src/containers/App.jsx
+++ client/src/containers/App.jsx
@@ -1,11 +1,15 @@
+/* eslint-disable no-shadow */
 import React from 'react';
 import { BrowserRouter, Route, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { fetchEvents } from '../actions/events';
+import { fetchFestivals } from '../actions/festivals';
 import Header from '../components/header';
 import MainJumbotron from '../components/jumbotron';
 import ResultsContainer from './resultsContainer';
 import './app.css';
 
-const App = () => {
+const App = ({ fetchEvents, fetchFestivals }) => {
   return (
     <BrowserRouter>
       <div>
@@ -15,8 +19,16 @@ const App = () => {
           path="/:category"
           render={({ match }) => (
             <React.Fragment>
-              <MainJumbotron match={match} />
-              <ResultsContainer match={match} />
+              <MainJumbotron
+                match={match}
+                fetchEvents={fetchEvents}
+                fetchFestivals={fetchFestivals}
+              />
+              <ResultsContainer
+                match={match}
+                fetchEvents={fetchEvents}
+                fetchFestivals={fetchFestivals}
+              />
             </React.Fragment>
           )}
         />
@@ -25,4 +37,12 @@ const App = () => {
   );
 };
 
-export default App;
+const mapDispatchToProps = {
+  fetchEvents,
+  fetchFestivals
+};
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(App);
