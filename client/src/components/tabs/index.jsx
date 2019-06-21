dd09fe7e0772f86045bc5da97a8ef88b0f17c249
Rodolfo Perez <rodolfobeste@gmail.com>
2019-06-21 23:49:04 +0300

    Added callbakcs to nav links

--- client/src/components/tabs/index.jsx
+++ client/src/components/tabs/index.jsx
@@ -1,20 +1,45 @@
 import React from 'react';
 import Nav from 'react-bootstrap/Nav';
+import { NavLink } from 'react-router-dom';
 
-const NavigationTabs = ({ category }) => {
+const NavigationTabs = ({ category, fetchEvents, fetchFestivals }) => {
+  const fetchNewResults = (cat, style = '') => {
+    if (category === 'events') {
+      fetchEvents(`/${cat}/${style}`);
+    } else if (category === 'festivals') {
+      fetchFestivals(`/${cat}/${style}`);
+    }
+  };
   return (
     <Nav fill justify variant="tabs" defaultActiveKey="/home" as="ul">
       <Nav.Item as="li">
-        <Nav.Link href={`/${category}`}>All</Nav.Link>
+        <NavLink to={`/${category}`} onClick={() => fetchNewResults(category)}>
+          All
+        </NavLink>
       </Nav.Item>
       <Nav.Item as="li">
-        <Nav.Link href={`/${category}/salsa`}>Salsa</Nav.Link>
+        <NavLink
+          to={`/${category}/salsa`}
+          onClick={() => fetchNewResults(category, 'salsa')}
+        >
+          Salsa
+        </NavLink>
       </Nav.Item>
       <Nav.Item as="li">
-        <Nav.Link href={`/${category}/bachata`}>Bachata</Nav.Link>
+        <NavLink
+          to={`/${category}/bachata`}
+          onClick={() => fetchNewResults(category, 'bachata')}
+        >
+          Bachata
+        </NavLink>
       </Nav.Item>
       <Nav.Item as="li">
-        <Nav.Link href={`/${category}/kizomba`}>Kizomba</Nav.Link>
+        <NavLink
+          to={`/${category}/kizomba`}
+          onClick={() => fetchNewResults(category, 'kizomba')}
+        >
+          Kizomba
+        </NavLink>
       </Nav.Item>
     </Nav>
   );
