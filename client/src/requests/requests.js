8885062c5f5f501d3f8ccb5d69ab20752810ee49
Rodolfo Perez <rodolfobeste@gmail.com>
2019-06-24 15:38:27 +0300

    Removed error handling in request

--- client/src/requests/requests.js
+++ client/src/requests/requests.js
@@ -4,13 +4,9 @@ import axios from 'axios';
  * @param object params Contains the parameters that will be sent in the request
  */
 const apiRequest = async params => {
-  try {
-    return await axios(`http://localhost:5000${params.endPoint}`, {
-      ...params
-    }).then(response => response.data);
-  } catch (e) {
-    return console.log('There was an error while fetching');
-  }
+  return axios(`http://localhost:5000${params.endPoint}`, {
+    ...params
+  }).then(response => response.data);
 };
 
 export default apiRequest;
