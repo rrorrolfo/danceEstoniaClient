95ac55332b45dcee55adb92141af90046a1cf35b
Rodolfo Perez <rodolfobeste@gmail.com>
2019-06-27 00:30:26 +0300

    Updated Redux dev tools extension

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -11,7 +11,7 @@ const store = createStore(
   appReducer,
   compose(
     applyMiddleware(thunk),
-    window.devToolsExtension && window.devToolsExtension()
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()
   )
 );
 
