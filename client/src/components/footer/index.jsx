45556496f6081dcf107971b6b114183f1e1570ab
Rodolfo Perez <rodolfobeste@gmail.com>
2019-07-11 23:28:19 +0300

    Created and styled footer component

---
+++ client/src/components/footer/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className="footer centered">
+      <p className="copyright">
+        &copy; 2019 DanceEstonia. All rights reserved |
+        <span>Crafted by DanceEstonia Dev.</span>
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
