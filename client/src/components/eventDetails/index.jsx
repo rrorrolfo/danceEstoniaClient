342f9cf6623d61ff6ea5e15e8a868be1a201dc53
Rodolfo Perez <rodolfobeste@gmail.com>
2019-07-13 01:14:43 +0300

    Added icons

--- client/src/components/eventDetails/index.jsx
+++ client/src/components/eventDetails/index.jsx
@@ -3,6 +3,14 @@ import { Container } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 import './eventDetails.css';
 import mapsApiKey from '../../config/config';
+import {
+  hotel,
+  calendar,
+  clock,
+  tickets,
+  website,
+  coupleDancing
+} from '../../assets/icons';
 
 const EventDetails = ({
   match,
@@ -36,28 +44,47 @@ const EventDetails = ({
           backgroundSize: 'cover'
         }}
       />
-      <Container className="event-details-container">
+      <Container className="event-details-main-container">
         <h1 className="event-title centered">{selectedEvent.name}</h1>
-        <p className="event-styles">{selectedEvent.styles.join(', ')}</p>
-        <p className="event-venue">{`${selectedEvent.venueOfEvent}, ${
-          selectedEvent.venueAddress
-        }, ${selectedEvent.cityOfEvent}, ${selectedEvent.countryOfEvent}`}</p>
-        <p className="event-date">{`${selectedEvent.dateOfEvent}, ${
-          selectedEvent.timeOfEvent
-        }`}</p>
-        <p className="event-social-links">
-          {selectedEvent.website
-            ? selectedEvent.website
-            : selectedEvent.fbEvent}
-        </p>
-        <p className="event-ticket-price">{selectedEvent.ticketPrice}</p>
+        <div className="event-info-container">
+          {coupleDancing()}
+          <p className="event-styles">{selectedEvent.styles.join(', ')}</p>
+        </div>
+        <div className="event-info-container">
+          {hotel()}
+          <p className="event-venue">
+            {`${selectedEvent.venueOfEvent}, ${selectedEvent.venueAddress}, ${
+              selectedEvent.cityOfEvent
+            }, ${selectedEvent.countryOfEvent}`}
+          </p>
+        </div>
+        <div className="event-info-container">
+          {calendar()}
+          <p className="event-date">{selectedEvent.dateOfEvent}</p>
+        </div>
+        <div className="event-info-container">
+          {clock()}
+          <p className="event-time">{selectedEvent.timeOfEvent}</p>
+        </div>
+        <div className="event-info-container">
+          {website()}
+          <p className="event-social-links">
+            {selectedEvent.website
+              ? selectedEvent.website
+              : selectedEvent.fbEvent}
+          </p>
+        </div>
+        <div className="event-info-container">
+          {tickets()}
+          <p className="event-ticket-price">{selectedEvent.ticketPrice}</p>
+        </div>
         <p className="event-description">{selectedEvent.description}</p>
         <iframe
           src={`https://www.google.com/maps/embed/v1/place?key=${mapsApiKey}
     &q=${formatMapSrc()}`}
-          title="test"
+          title={`${selectedEvent.name} map`}
           width="100%"
-          height="450"
+          height="400"
           frameBorder="0"
           allowFullScreen
         />
