fd308de6cd23b959cb8cad0b602938b22b7eea36
Rodolfo Perez <rodolfobeste@gmail.com>
2019-07-20 02:16:30 +0300

    Created request for node mailer

--- client/src/requests/requests.js
+++ client/src/requests/requests.js
@@ -3,10 +3,26 @@ import axios from 'axios';
 /**
  * @param object params Contains the parameters that will be sent in the request
  */
-const apiRequest = async params => {
+export const apiRequest = params => {
   return axios(`http://localhost:5000${params.endPoint}`, {
     ...params
   }).then(response => response.data);
 };
 
-export default apiRequest;
+const requestOptions = {
+  headers: {
+    'Content-Type': 'application/json'
+  }
+};
+/**
+ * @param object data Email body data
+ */
+export const emailRequest = data => {
+  return axios
+    .post(
+      'https://node-mailer-rcph.herokuapp.com/contact',
+      data,
+      requestOptions
+    )
+    .then(response => response.data);
+};
