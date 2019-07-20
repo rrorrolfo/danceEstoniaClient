5150f870898acf13fbf23108e0f3c99c76663b2c
Rodolfo Perez <rodolfobeste@gmail.com>
2019-07-20 15:01:38 +0300

    Optimized styling

--- client/src/components/notFound/index.jsx
+++ client/src/components/notFound/index.jsx
@@ -1,23 +1,28 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
+import Footer from '../footer';
 import './notFound.css';
 
 const NotFound = () => {
   return (
-    <Container className="not-found-container centered">
-      <h2>Oops! We couldn´t find the page you are looking for...</h2>
-      <Container>
-        <ul className="back-to-nav">
-          <li className="category-cta">
-            <NavLink to="/events">Back to parties & events</NavLink>
-          </li>
-          <li className="category-cta">
-            <NavLink to="/festivals">Back to festivals</NavLink>
-          </li>
-        </ul>
+    <React.Fragment>
+      <Container className="not-found-container centered full-vh-height">
+        <h2>404</h2>
+        <h2>Oops! We couldn´t find the page you are looking for...</h2>
+        <Container>
+          <ul className="back-to-nav">
+            <li className="category-cta">
+              <NavLink to="/events">Back to parties & events</NavLink>
+            </li>
+            <li className="category-cta">
+              <NavLink to="/festivals">Back to festivals</NavLink>
+            </li>
+          </ul>
+        </Container>
       </Container>
-    </Container>
+      <Footer />
+    </React.Fragment>
   );
 };
 
