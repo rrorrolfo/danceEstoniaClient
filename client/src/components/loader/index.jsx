c2b8071768eccb3eff996c1abcb4937fd6266650
Rodolfo Perez <rodolfobeste@gmail.com>
2019-07-27 23:41:03 +0300

    Created and styled loader

---
+++ client/src/components/loader/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Loader = () => {
+  return (
+    <div className="main-loader">
+      <div className="loader-logo-container centered">
+        <h2>Ready to dance?</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Loader;
