5f8f1257d1b34a5b3d39f2fa53045e6809edefe4
Rodolfo Perez <rodolfobeste@gmail.com>
2019-07-27 23:42:07 +0300

    Connected app to redux and added Loader

--- client/src/containers/App.jsx
+++ client/src/containers/App.jsx
@@ -1,18 +1,92 @@
-import React from 'react';
+/* eslint-disable no-shadow */
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import {
+  fetchEvents,
+  fetchEventsByStyle,
+  fetchSingleEvent
+} from '../actions/events';
+import {
+  fetchFestivals,
+  fetchFestivalsByStyle,
+  fetchSingleFestival
+} from '../actions/festivals';
 import Header from '../components/header';
 import AppRoutes from '../routes';
+import Loader from '../components/loader';
 import './app.css';
 
-const App = () => {
+const App = ({
+  fetchEvents,
+  fetchEventsByStyle,
+  fetchFestivals,
+  fetchFestivalsByStyle,
+  fetchSingleEvent,
+  fetchSingleFestival,
+  singleEvent,
+  singleFestival
+}) => {
+  const [isLoading, toggleLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => toggleLoading(!isLoading), 2000);
+  }, []);
   return (
     <BrowserRouter>
       <div>
         <Header />
-        <AppRoutes />
+        <AppRoutes
+          fetchEvents={fetchEvents}
+          fetchEventsByStyle={fetchEventsByStyle}
+          fetchFestivals={fetchFestivals}
+          fetchSingleEvent={fetchSingleEvent}
+          singleEvent={singleEvent}
+          fetchFestivalsByStyle={fetchFestivalsByStyle}
+          fetchSingleFestival={fetchSingleFestival}
+          singleFestival={singleFestival}
+        />
+        {isLoading ? <Loader /> : null}
       </div>
     </BrowserRouter>
   );
 };
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    singleEvent: state.events.singleEvent,
+    singleFestival: state.festivals.singleFestival
+  };
+};
+
+const mapDispatchToProps = {
+  fetchEvents,
+  fetchEventsByStyle,
+  fetchFestivals,
+  fetchFestivalsByStyle,
+  fetchSingleEvent,
+  fetchSingleFestival
+};
+
+App.propTypes = {
+  fetchEvents: PropTypes.func.isRequired,
+  fetchEventsByStyle: PropTypes.func.isRequired,
+  fetchFestivals: PropTypes.func.isRequired,
+  fetchFestivalsByStyle: PropTypes.func.isRequired,
+  fetchSingleEvent: PropTypes.func.isRequired,
+  fetchSingleFestival: PropTypes.func.isRequired,
+  // eslint-disable-next-line react/forbid-prop-types
+  singleEvent: PropTypes.object,
+  // eslint-disable-next-line react/forbid-prop-types
+  singleFestival: PropTypes.object
+};
+
+App.defaultProps = {
+  singleEvent: null,
+  singleFestival: null
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
