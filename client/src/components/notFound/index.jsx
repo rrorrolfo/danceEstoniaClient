25bb7ca2f2288e966688f885e27e5e01d84bde5f
Rodolfo Perez <rodolfobeste@gmail.com>
2019-08-17 23:00:13 +0300

    Added logic for displaying message according to error status

--- client/src/components/notFound/index.jsx
+++ client/src/components/notFound/index.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
+import PropTypes from 'prop-types';
 import Footer from '../footer';
 import './notFound.css';
 
-const NotFound = () => {
+const NotFound = ({ history }) => {
+  const { errorStatus } = history.location.state || 404;
   return (
     <React.Fragment>
       <Container className="not-found-container centered full-vh-height">
-        <h2>404</h2>
-        <h2>Oops! We couldn´t find the page you are looking for...</h2>
+        <h2>{errorStatus === undefined ? 404 : errorStatus}</h2>
+        <h2>
+          {errorStatus === 404 || errorStatus === undefined
+            ? 'Oops! We couldn´t find the page you are looking for...'
+            : 'Oops! something went wrong!'}
+        </h2>
         <Container>
           <ul className="back-to-nav">
             <li className="category-cta">
@@ -24,6 +30,19 @@ const NotFound = () => {
       <Footer />
     </React.Fragment>
   );
+};
+
+NotFound.propTypes = {
+  // eslint-disable-next-line react/forbid-prop-types
+  history: PropTypes.object
+};
+
+NotFound.defaultProps = {
+  history: {
+    location: {
+      state: { errorStatus: 500 }
+    }
+  }
 };
 
 export default NotFound;
