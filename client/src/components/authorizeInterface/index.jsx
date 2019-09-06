aa3348f5761b8f6cc1e7ac44708d3d15e665f09a
Rodolfo Perez <rodolfobeste@gmail.com>
2019-09-06 23:28:26 +0300

    Created authorization interface component

---
+++ client/src/components/authorizeInterface/index.jsx
@@ -0,0 +1,72 @@
+import React, { useEffect, useState } from 'react';
+import { Container } from 'react-bootstrap';
+import TimeFrameGroup from '../timeframeGroup';
+import { apiRequest } from '../../requests/requests';
+
+const AuthInterface = () => {
+  const [eventsResults, updateEventsResults] = useState([]);
+  const [festivalsResults, updateFestivalsResults] = useState([]);
+
+  useEffect(() => {
+    apiRequest({ endPoint: '/events', unauthorized: 1 })
+      .then(response => {
+        updateEventsResults(response);
+      })
+      .catch(error => console.log(error));
+    apiRequest({ endPoint: '/festivals', unauthorized: 1 })
+      .then(response => updateFestivalsResults(response))
+      .catch(error => console.log(error));
+  }, []);
+
+  const displayTimeFramecontainers = (
+    resultsByGroup,
+    timeFrame = 'month',
+    category
+  ) => {
+    return resultsByGroup.map(group => (
+      <TimeFrameGroup
+        dateHappening={timeFrame === 'week' ? group._id.week : group._id.month}
+        timeFrame={timeFrame}
+        events={group.records}
+        category={category}
+        isAdmin
+        key={
+          timeFrame === 'week'
+            ? `${group._id.week}-${Math.random()}`
+            : `${group._id.month}-${Math.random()}`
+        }
+      />
+    ));
+  };
+
+  return (
+    <Container className="results-container">
+      <Container>
+        <h2 className="centered" style={{ marginTop: '65px' }}>
+          - Unauthorized Events -
+        </h2>
+        {eventsResults.length ? (
+          displayTimeFramecontainers(eventsResults, 'week', 'events')
+        ) : (
+          <h3 className="centered" style={{ marginTop: '25px' }}>
+            No past event results were obtained
+          </h3>
+        )}
+      </Container>
+      <Container>
+        <h2 className="centered" style={{ marginTop: '65px' }}>
+          - Unauthorized Festivals -
+        </h2>
+        {festivalsResults.length ? (
+          displayTimeFramecontainers(festivalsResults, 'month', 'festivals')
+        ) : (
+          <h3 className="centered" style={{ marginTop: '25px' }}>
+            No past festival results were obtained
+          </h3>
+        )}
+      </Container>
+    </Container>
+  );
+};
+
+export default AuthInterface;
