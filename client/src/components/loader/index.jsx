c090ceae709c25a2a9831c85a5abf0e8420f81ca
Rodolfo Perez <rodolfobeste@gmail.com>
2019-09-11 01:56:44 +0300

    Added Dance Estonia blue logo

--- client/src/components/loader/index.jsx
+++ client/src/components/loader/index.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { danceEstoniaBl } from '../../assets/icons/Logos';
 
 const Loader = () => {
   return (
     <div className="main-loader">
-      <div className="loader-logo-container centered">
-        <h2>Ready to dance?</h2>
+      <div className="loader-logo-container">
+        <div>
+          {danceEstoniaBl(
+            'danceEstoniaBl-container',
+            'danceEstoniaBl-logo-group'
+          )}
+        </div>
+        <h2 className="teaser centered">Ready to dance?</h2>
       </div>
     </div>
   );
