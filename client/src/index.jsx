32f72bb462e2c05fd5d5e6bad5014475e473fd19
Rodolfo Perez <rodolfobeste@gmail.com>
2019-09-15 23:06:58 +0300

    Removed CRA comments

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -6,7 +6,6 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import App from './containers/App';
 import appReducer from './reducers';
-/* import * as serviceWorker from './serviceWorker/serviceWorker'; */
 
 const store = createStore(
   appReducer,
@@ -22,8 +21,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-/* serviceWorker.unregister(); */
