5a620501891dd3994781df383526c2225ba27485
Rodolfo Perez <rodolfobeste@gmail.com>
2019-09-18 00:57:42 +0300

    Added loading text and optimized classes

--- client/src/components/loader/index.jsx
+++ client/src/components/loader/index.jsx
@@ -5,9 +5,10 @@ import { danceEstoniaBl } from '../../assets/icons/Logos';
 const Loader = ({ text }) => {
   return (
     <div className="main-loader">
-      <div className="loader-logo-container">
-        <h2 className="teaser centered">{text}</h2>
-        <div>
+      <div className="loader-logo-container centered">
+        <h2 className="teaser">{text}</h2>
+        <h4 className="loading">Loading...</h4>
+        <div className="centered">
           {danceEstoniaBl(
             'danceEstoniaBl-container',
             'danceEstoniaBl-logo-group'
