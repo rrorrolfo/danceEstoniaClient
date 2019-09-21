bc5b977c88dcc7ff407919afc8834edcb840264e
Rodolfo Perez <rodolfobeste@gmail.com>
2019-09-21 15:46:14 +0300

    Created interface component for managing all events and festivals

---
+++ client/src/components/allEvents/index.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import ResultsContainer from '../../containers/resultsContainer';
+
+const AllEventsManager = ({
+  isAdmin,
+  match,
+  toggleLoader,
+  updateLoaderText
+}) => {
+  return (
+    <React.Fragment>
+      <div style={{ marginTop: '100px' }}>
+        <ResultsContainer
+          isAdmin={isAdmin}
+          topLevelMatch={match}
+          category="events"
+          toggleLoader={toggleLoader}
+          updateLoaderText={updateLoaderText}
+        />
+        <ResultsContainer
+          isAdmin={isAdmin}
+          topLevelMatch={match}
+          category="festivals"
+          toggleLoader={toggleLoader}
+          updateLoaderText={updateLoaderText}
+        />
+      </div>
+    </React.Fragment>
+  );
+};
+
+AllEventsManager.propTypes = {
+  isAdmin: PropTypes.bool,
+  match: PropTypes.bool,
+  toggleLoader: PropTypes.func,
+  updateLoaderText: PropTypes.func
+};
+
+AllEventsManager.defaultProps = {
+  isAdmin: false,
+  match: null,
+  toggleLoader: null,
+  updateLoaderText: null
+};
+
+export default AllEventsManager;
