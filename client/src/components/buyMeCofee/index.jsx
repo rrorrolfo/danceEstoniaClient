451e761f4f640ae326cce943335497340efa96b4
Rodolfo Perez <rodolfobeste@gmail.com>
2019-09-29 00:42:34 +0300

    Added "support us" text

--- client/src/components/buyMeCofee/index.jsx
+++ client/src/components/buyMeCofee/index.jsx
@@ -4,18 +4,32 @@ import './buyMeCofee.css';
 
 const BuyMeCofeeCTA = ({ customClass }) => {
   return (
-    <a
-      className={`bmc-button ${customClass}`}
-      target="_blank"
-      rel="noopener noreferrer"
-      href="https://www.buymeacoffee.com/qDN7sWBi5"
-    >
-      <img
-        src="https://bmc-cdn.nyc3.digitaloceanspaces.com/BMC-button-images/BMC-btn-logo.svg"
-        alt="Buy me a coffee"
-      />
-      <span style={{ marginLeft: '5px' }}>Buy me a coffee</span>
-    </a>
+    <>
+      <a
+        className={`bmc-button ${customClass}`}
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://www.buymeacoffee.com/qDN7sWBi5"
+      >
+        <img
+          src="https://bmc-cdn.nyc3.digitaloceanspaces.com/BMC-button-images/BMC-btn-logo.svg"
+          alt="Buy me a coffee"
+        />
+        <span style={{ marginLeft: '5px' }}>Buy me a coffee</span>
+        <span
+          clasName="support-text"
+          style={{
+            position: 'absolute',
+            bottom: '-40px',
+            left: '45px',
+            width: '90px',
+            color: '#0072ce'
+          }}
+        >
+          Support us
+        </span>
+      </a>
+    </>
   );
 };
 
