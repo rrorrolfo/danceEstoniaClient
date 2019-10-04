f57d7284d8a56cb0be6e361036f424e80bead46e
Rodolfo Perez <rodolfobeste@gmail.com>
2019-10-04 23:32:34 +0300

    Added translations

---
+++ client/src/translations/index.js
@@ -0,0 +1,129 @@
+/* eslint-disable camelcase */
+export const t__eng = {
+  general: {
+    events: 'events',
+    festivals: 'festivals',
+    partiesandEvents: 'Parties & Events',
+    all: 'All'
+  },
+  header: {
+    createEvent: 'Create Event',
+    contact: 'Contact'
+  },
+  jumbotron: {
+    titleEvents: 'Find the best dancing events in Estonia',
+    titleFestivals: 'Find the best dancing festivals in Estonia'
+  },
+  body: {
+    upcomingEvents: 'Upcoming Parties and Events in Estonia',
+    upcomingFestivals: 'Upcoming Festivals in Europe',
+    noResultsEvents: 'Sorry!, We couldn´t find any events, please check later.',
+    noResultsFestivals:
+      'Sorry!, We couldn´t find any festivals, please check later.'
+  },
+  contact: {
+    getInContact: 'Get in contact with us',
+    message: 'Message',
+    sendMessage: 'Send Message'
+  },
+  form: {
+    title: 'Fill the following form to create your event.',
+    kindOfEvent: 'What kind of event you want to create?',
+    kindOfEventValidation:
+      'Please select the type of event you want to create.',
+    dancingStyles: 'What dancing style(s) is the event?',
+    dancingStylesValidation: 'Please select at least one dancing style.',
+    nameOfEvent: 'Name of the event',
+    nameOfEventValidation: 'Please provide a name for the event.',
+    dateOfEvent: 'Date of the event',
+    endDate: 'End date of the festival',
+    startTime: 'Time the event starts',
+    ticketPrice: 'Ticket Price',
+    currency: 'Currency',
+    ticketLink: 'Link to buy tickets',
+    venueName: 'Venue name',
+    venuePlaceholder: 'Venue of the festival',
+    venueNameValidation: 'Please add the name of the venue.',
+    venueAddress: 'Venue address',
+    venueAddressPlaceholder: 'Address of the festival venue',
+    city: 'City',
+    cityPlaceholder: 'e.g.Tallinn',
+    cityValidation: 'Please add the city where the event will take place.',
+    website: 'Festival website',
+    websitePlaceholder: 'Website URL (e.g. www.example.com)',
+    fbEvent: 'Facebook event',
+    description: 'Write the description of the event',
+    image: 'Select an image to be displayed with the event',
+    imageValidation: 'Please add an image for the event.',
+    freeFestival: 'Festival is free',
+    createFestival: 'Create festival',
+    createEvent: 'Create event',
+    successMessage: 'do it in the file?',
+    successMessage2: 'This process should take only a few minutes.'
+  }
+};
+
+export const t__est = {
+  general: {
+    events: 'üritused',
+    festivals: 'festivalid',
+    partiesandEvents: 'Peod & Üritused',
+    all: 'Kõik'
+  },
+  header: {
+    createEvent: 'Loo Üritus',
+    contact: 'Kontact'
+  },
+  jumbotron: {
+    titleEvents: 'Leia Eesti parimaid tantsupidusid',
+    titleFestivals: 'Leia Eesti parimaid tantsufestivalid'
+  },
+  body: {
+    upcomingEvents: 'Tulevased peod & üritused Eestis',
+    upcomingFestivals: 'Tulevased festivalid Eestis',
+    noResultsEvents:
+      'Vabandame! Üritusi ei leitud. Palun proovige hiljem uuesti',
+    noResultsFestivals:
+      'Vabandame! Festivalid ei leitud. Palun proovige hiljem uuesti'
+  },
+  contact: {
+    getInContact: 'Võta meiega ühendust',
+    message: 'Sõnum',
+    sendMessage: 'Saada Sõnum'
+  },
+  form: {
+    title: 'Täida järgnev vorm ürituse loomiseks',
+    kindOfEvent: 'Millist üritust soovite luua?',
+    kindOfEventValidation: 'Palun valige, millist üritust soovite luua.',
+    dancingStyles: 'Millistest tantsustiilidest üritus koosneb?',
+    dancingStylesValidation: 'Palun valige, millist üritust soovite luua.',
+    nameOfEvent: 'Ürituse nimi',
+    nameOfEventValidation: 'Palun sisestage ürituse nimi.',
+    dateOfEvent: 'Toimumisaeg',
+    endDate: 'Lõppaeg',
+    startTime: 'Algus',
+    ticketPrice: 'Piletihind',
+    currency: 'Valuuta',
+    ticketLink: 'Piletiostu aadress (link)',
+    venueName: '',
+    venuePlaceholder: '',
+    venueNameValidation: '',
+    venueAddress: '',
+    venueAddressPlaceholder: '',
+    city: 'Linn',
+    cityPlaceholder: 'nt.Tallinn',
+    cityValidation: 'Palun sisestage linn kus üritus aset võtab.',
+    website: 'Festivali koduleht',
+    websitePlaceholder: 'kodulehe aadress (nt. www.example.com)',
+    fbEvent: 'Facebooki üritus',
+    description: 'Sisestage festivali kirjeldus',
+    image: 'Vali pilt, mida festivalil kuvada',
+    imageValidation: 'Palun lisage festivalile pilt',
+    freeFestival: 'Osalemine on tasuta?',
+    createFestival: 'Loo festival',
+    createEvent: 'Loo üritus',
+    successMessage:
+      'Täname festivali loomise eest! Festival {nameOfEvent} vaadatakse meie meeskonna poolt üle ning kui kõik klapib siis see avaldatakse Dance Estonias ',
+    successMessage2: 'See võtab tavaliselt mõne minuti'
+  }
+};
