42681da4a6ecfe0dbaf620443c5943587fede72d
Rodolfo Perez <rodolfobeste@gmail.com>
2019-10-06 16:27:01 +0300

    Updated translations texts

--- client/src/translations/index.js
+++ client/src/translations/index.js
@@ -1,10 +1,22 @@
 /* eslint-disable camelcase */
 export const t__eng = {
   general: {
-    events: 'events',
-    festivals: 'festivals',
+    events: 'Events',
+    event: 'Event',
+    party: 'Party',
+    festivals: 'Festivals',
+    festival: 'Festival',
+    eventOrParty: 'Event or Party',
     partiesandEvents: 'Parties & Events',
-    all: 'All'
+    all: 'All',
+    estonia: 'Estonia',
+    inEstonia: 'in Estonia',
+    europe: 'Europe',
+    inEurope: 'in Europe'
+  },
+  loader: {
+    teaser: 'Ready to dance?',
+    loading: 'Loading'
   },
   header: {
     createEvent: 'Create Event',
@@ -15,6 +27,7 @@ export const t__eng = {
     titleFestivals: 'Find the best dancing festivals in Estonia'
   },
   body: {
+    upcoming: 'Upcoming',
     upcomingEvents: 'Upcoming Parties and Events in Estonia',
     upcomingFestivals: 'Upcoming Festivals in Europe',
     noResultsEvents: 'Sorry!, We couldn´t find any events, please check later.',
@@ -23,8 +36,21 @@ export const t__eng = {
   },
   contact: {
     getInContact: 'Get in contact with us',
+    name: 'Name',
+    nameValidation: 'Please type your name.',
+    email: 'Email',
+    emailPlaceholder: 'Enter your email',
+    emailValidation:
+      'Your email must have an @ and a domain (e.g. email@example.com).',
     message: 'Message',
-    sendMessage: 'Send Message'
+    messageValidation: 'Your message must be at least 20 letters long.',
+    sendMessage: 'Send Message',
+    sending: 'Sending...',
+    messageSent: 'Your message has been sent successfully!',
+    messageSent2:
+      'Thanks for contacting us, we will get in touch with you as soon as possible.',
+    messageSentError:
+      'Oops! There was an error while sending your message, please try again.'
   },
   form: {
     title: 'Fill the following form to create your event.',
@@ -49,6 +75,7 @@ export const t__eng = {
     city: 'City',
     cityPlaceholder: 'e.g.Tallinn',
     cityValidation: 'Please add the city where the event will take place.',
+    country: 'Country',
     website: 'Festival website',
     websitePlaceholder: 'Website URL (e.g. www.example.com)',
     fbEvent: 'Facebook event',
@@ -56,8 +83,8 @@ export const t__eng = {
     image: 'Select an image to be displayed with the event',
     imageValidation: 'Please add an image for the event.',
     freeFestival: 'Festival is free',
-    createFestival: 'Create festival',
-    createEvent: 'Create event',
+    create: 'Create',
+    update: 'Update',
     successMessage: 'do it in the file?',
     successMessage2: 'This process should take only a few minutes.'
   }
@@ -65,10 +92,22 @@ export const t__eng = {
 
 export const t__est = {
   general: {
-    events: 'üritused',
-    festivals: 'festivalid',
+    events: 'Üritused',
+    event: 'Sündmus',
+    party: 'Party',
+    festivals: 'Festivalid',
+    festival: 'Festival',
+    eventOrParty: 'Sündmus/Pidu',
     partiesandEvents: 'Peod & Üritused',
-    all: 'Kõik'
+    all: 'Kõik',
+    estonia: 'Eesti',
+    inEstonia: 'Eestis',
+    europe: 'Europa',
+    inEurope: 'Europas'
+  },
+  loader: {
+    loading: 'Loading',
+    teaser: 'Valmis tantsima?'
   },
   header: {
     createEvent: 'Loo Üritus',
@@ -79,17 +118,31 @@ export const t__est = {
     titleFestivals: 'Leia Eesti parimaid tantsufestivalid'
   },
   body: {
+    upcoming: 'Tulevased',
     upcomingEvents: 'Tulevased peod & üritused Eestis',
     upcomingFestivals: 'Tulevased festivalid Eestis',
     noResultsEvents:
-      'Vabandame! Üritusi ei leitud. Palun proovige hiljem uuesti',
+      'Vabandame! Üritusi ei leitud. Palun proovige hiljem uuesti.',
     noResultsFestivals:
-      'Vabandame! Festivalid ei leitud. Palun proovige hiljem uuesti'
+      'Vabandame! Festivalid ei leitud. Palun proovige hiljem uuesti.'
   },
   contact: {
     getInContact: 'Võta meiega ühendust',
+    name: 'Nimi',
+    nameValidation: 'Palun kirjutage oma nimi.',
+    email: 'E-post',
+    emailPlaceholder: 'Sisestage oma e-posti aadress',
+    emailValidation:
+      'Teie e-posti aadressil peab olema "@" ja domeen (nt. email@example.com).',
     message: 'Sõnum',
-    sendMessage: 'Saada Sõnum'
+    messageValidation: 'Teie sõnum peab olema vähemalt 20 tähte pikk',
+    sendMessage: 'Saada Sõnum',
+    sending: 'Saatmine...',
+    messageSent: 'Sinu sõnum on edukalt saadetud!',
+    messageSent2:
+      'Täname, et võtsite meiega ühendust, võtame teiega ühendust nii kiiresti kui võimalik.',
+    messageSentError:
+      'Oops!, Teie sõnumi saatmisel ilmnes viga, Palun proovi uuesti'
   },
   form: {
     title: 'Täida järgnev vorm ürituse loomiseks',
@@ -105,14 +158,15 @@ export const t__est = {
     ticketPrice: 'Piletihind',
     currency: 'Valuuta',
     ticketLink: 'Piletiostu aadress (link)',
-    venueName: '',
-    venuePlaceholder: '',
-    venueNameValidation: '',
-    venueAddress: '',
-    venueAddressPlaceholder: '',
+    venueName: 'Koht',
+    venuePlaceholder: 'Koht nimi',
+    venueNameValidation: 'Palun lisage koha nimi',
+    venueAddress: 'Koha aadress',
+    venueAddressPlaceholder: 'Sündmuse koha aadress',
     city: 'Linn',
     cityPlaceholder: 'nt.Tallinn',
     cityValidation: 'Palun sisestage linn kus üritus aset võtab.',
+    country: 'Riik',
     website: 'Festivali koduleht',
     websitePlaceholder: 'kodulehe aadress (nt. www.example.com)',
     fbEvent: 'Facebooki üritus',
@@ -120,10 +174,10 @@ export const t__est = {
     image: 'Vali pilt, mida festivalil kuvada',
     imageValidation: 'Palun lisage festivalile pilt',
     freeFestival: 'Osalemine on tasuta?',
-    createFestival: 'Loo festival',
-    createEvent: 'Loo üritus',
+    create: 'Loo',
+    update: 'Ajakohastama',
     successMessage:
-      'Täname festivali loomise eest! Festival {nameOfEvent} vaadatakse meie meeskonna poolt üle ning kui kõik klapib siis see avaldatakse Dance Estonias ',
+      'Täname üritus loomise eest! {nameOfEvent} vaadatakse meie meeskonna poolt üle ning kui kõik klapib siis see avaldatakse Dance Estonias ',
     successMessage2: 'See võtab tavaliselt mõne minuti'
   }
 };
